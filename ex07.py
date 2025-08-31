051cf16316f381001a4fbee1ceddc098115276f0
Clodoaldo Junior <156717109+C-Juniorr@users.noreply.github.com>
2025-08-31 11:45:43 -0300

    Update ex07.py

--- ex07.py
+++ ex07.py
@@ -1,6 +1,6 @@
 
 idade_motorista = int(input("Qual a sua idade? "))
-tem_carteira_str = input("Você tem carteira de motorista? (True/False ou Sim/Nao) ")
+tem_carteira_str = input("Você tem carteira de motorista? (True/False) ")
 tem_carteira = parse_bool(tem_carteira_str)
 
 pode_dirigir = (idade_motorista >= 18) and tem_carteira

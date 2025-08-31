a046e940eb186d3494cea501903ff05076d9edf9
Clodoaldo Junior <156717109+C-Juniorr@users.noreply.github.com>
2025-08-31 11:45:19 -0300

    Update ex07.py

--- ex07.py
+++ ex07.py
@@ -1,8 +1,4 @@
 
-def parse_bool(s: str) -> bool:
-    s = s.strip().lower()
-    return s in {"true","t","1","sim","s","y","yes"}
-
 idade_motorista = int(input("Qual a sua idade? "))
 tem_carteira_str = input("Você tem carteira de motorista? (True/False ou Sim/Nao) ")
 tem_carteira = parse_bool(tem_carteira_str)
